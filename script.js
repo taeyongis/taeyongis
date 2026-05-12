97efa330e88e108c5a3710578265d8898e9e4f85
taeyongis <tycultureclub@gmail.com>
2026-05-11 22:09:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("WYLD site loaded");
+const words = [
+  "ART",
+  "NEO",
+  "SHALALA",
+  "TAP",
+  "VISIONARY",
+  "EXPERIMENTAL",
+  "WYLD"
+];
+
+let index = 0;
+
+const changingWord = document.getElementById("changing-word");
+
+setInterval(() => {
+
+  index = (index + 1) % words.length;
+
+  changingWord.textContent = words[index];
+
+}, 2000);
