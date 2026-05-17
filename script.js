036b534baa1148a471997740c3b0ae94ce1e2a13
taeyongis <tycultureclub@gmail.com>
2026-05-17 00:42:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function () {
 
+  /* WORD ANIMATION */
+
   const words = [
     "ART",
     "NEO",
@@ -24,5 +26,29 @@ window.onload = function () {
 
   }, 1500);
 
+  /* CAROUSEL */
+
+  const avatarsContainer = document.querySelector(".avatars");
+
+  const leftArrow = document.querySelector(".left-arrow");
+  const rightArrow = document.querySelector(".right-arrow");
+
+  rightArrow.addEventListener("click", () => {
+
+    const firstAvatar = avatarsContainer.firstElementChild;
+
+    avatarsContainer.appendChild(firstAvatar);
+
+  });
+
+  leftArrow.addEventListener("click", () => {
+
+    const avatars = avatarsContainer.children;
+
+    const lastAvatar = avatars[avatars.length - 1];
+
+    avatarsContainer.prepend(lastAvatar);
+
+  });
+
 };
-}, 1500);
